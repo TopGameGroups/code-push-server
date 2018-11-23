cc39cf8572e0dcc4c9ad9307d7e816f45b520526
heronlyj <heronlyj@gmail.com>
2018-11-23 11:43:22 +0800

    update format

--- routes/account.js
+++ routes/account.js
@@ -6,13 +6,15 @@ var security = require('../core/utils/security');
 var middleware = require('../core/middleware');
 
 router.get('/', middleware.checkToken, (req, res) => {
-  var userInfo = {
-    email:req.users.email,
-    id:req.users.identical,
-    linkedProviders: [],
-    name:req.users.username,
-  };
-  res.send({account:userInfo});
+    var userInfo = {
+        email: req.users.email,
+        id: req.users.identical,
+        linkedProviders: [],
+        name: req.users.username,
+    };
+    res.send({
+        account: userInfo
+    });
 });
 
 module.exports = router;
